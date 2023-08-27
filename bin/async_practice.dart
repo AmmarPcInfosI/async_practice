5630c35e3c318e090365e43eb567b8f867efb395
Ammar Al Hasani <ammarsalem2011@gmail.com>
2023-08-27 09:53:33 +0400

    Initial commit

---
+++ bin/async_practice.dart
@@ -0,0 +1,41 @@
+import 'package:async_practice/async_practice.dart' as async_practice;
+
+void main() async{
+  double a=12.3;
+  double b=6.2;
+  Future f=Future(() => 5);
+  Future.delayed(Duration(seconds: 5),(){
+    print("The sum operation of $a & $b =${sum(a, b)}");
+  });
+  Future.delayed(Duration(seconds: 4),(){
+    print("The divide operation of $a & $b =${divide(a, b)}");
+  });
+  Future.delayed(Duration(seconds: 3),(){
+    print("The multiply operation of $a & $b =${multiply(a, b)}");
+  });
+  Future.delayed(Duration(seconds: 2),(){
+    print("The subtract operation of $a & $b =${sub(a, b)}");
+  });
+  Future.delayed(Duration(seconds: 2),(){
+    print("The average operation of $a & $b =${avg(a, b)}");
+  });
+  
+
+}
+
+double sum(double a,double b){
+  return (a+b);
+}
+double divide(double a,double b){
+  return (a/b);
+}
+double multiply(double a,double b){
+  return (a*b);
+}
+double sub(double a,double b){
+  return (a-b);
+}
+double avg(double a,double b){
+  return (a+b)/2;
+}
+
---
+++ test/async_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:async_practice/async_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
